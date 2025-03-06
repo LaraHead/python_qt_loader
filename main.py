4d5fd3e4e8f4c58b503206f4043f25d4e164fa1b
LaraHead <laragolov@gmail.com>
2025-03-06 23:39:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -98,11 +98,11 @@ class MainWindow(QWidget):
     def connDb1(self):
         if not self.isConnected :
             # connect  to db
-            database_username = 'DB2ADMIN'
-            database_password = 'gfhfljrc9'
-            database_hostname = '10.2.0.20'
+            database_username = 'user'
+            database_password = 'pass'
+            database_hostname = '127.0.0.0'
             database_port = '50000'
-            database_name = 'OPFR002'
+            database_name = 'dbname'
             # Construct the connection string
             connection_string = (
                 f"DATABASE={database_name};"
